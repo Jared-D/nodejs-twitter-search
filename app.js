bfaf2679fc8ad551229fb62da3a8a60f15274fca
Jared-D <jared42d@gmail.com>
2017-05-18 17:01:32 +1000

    Add skeleton stubs

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 var Twitter = require('twitter');
-var client_config = require('./config.json');
-//var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database('db.sqlite');
+var twitter_config = require('./config.json');
+var sqlite3 = require('sqlite3').verbose();
 
-var client = new Twitter(client_config);
+// Create twitter client.
+var twitter_client = new Twitter(twitter_config);
 
-client.get('search/tweets', {q: '#liveperson'}, function(error, tweets, response) {
-       console.log(tweets);
+// Open Sqlite DB.
+var db = new sqlite3.Database('db.sqlite');
+
+// Query twitter.
+twitter_client.get('search/tweets', {q: '#liveperson'}, function(error, tweets, response) {
+//    console.log(tweets);
+    if(!("statuses" in tweets)) {
+        throw new Error("'statuses' key missing from result");
+    }
+    db.serialize(function() {
+        init_db();
+        var statuses = tweets.statuses;
+        for(i in statuses) {
+            save_status(statuses[i]);
+        }
+    });
 });
 
+// Initialise DB if not already done.
+function init_db() {
+//    db.run("CREATE TABLE IF NOT EXISTS tweets (id INTEGER PRIMARY KEY, text TEXT)");
+}
+
+// Save Status to DB.
+function save_status(data) {
+    // Convert twitter date to Date object,
+    // see http://programming.mvergel.com/2013/03/convert-twitters-createdat-to.html
+//    created_date = new Date(Date.parse(data.created_at.replace(/( \+)/, ' UTC$1')));
+//    created_date.toISOString();
+    console.log(data.id);
+//    db.run("INSERT INTO tweets ()");
+}
